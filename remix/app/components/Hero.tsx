000c071843ba3825d6494c73de7307e472e22530
Xeroneon <andrewvasquez0113@gmail.com>
2024-08-03 10:41:24 -0700

    added more content

--- remix/app/components/Hero.tsx
+++ remix/app/components/Hero.tsx
@@ -2,31 +2,43 @@ import logo from "../images/Excel-small-logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { useLoaderData } from "@remix-run/react";
 
 export default function Hero() {
+  const data = useLoaderData() as any;
+  const { Phone, SupportEmail } = data.contactInfo.attributes;
   return (
     <div className="flex p-8 items-center justify-between text-gray-600">
       <div className="flex items-center">
         <img src={logo} className="mx-5" />
         <div>
-          <h1 className="text-4xl kadwa-bold ">
-            Excel Christian School
-          </h1>
+          <h1 className="text-4xl kadwa-bold ">Excel Christian School</h1>
           <h4 className="text-lg">Excellence & Faith in Education</h4>
         </div>
       </div>
       <div className="flex justify-between items-center gap-10">
         <div className="flex gap-4">
-          <FontAwesomeIcon icon={faPhone} size="3x" className="text-red-800" />
-          <h2 className="kadwa-regular text-xl ">Phone Number</h2>
+          <FontAwesomeIcon icon={faPhone} size="2x" className="text-red-800" />
+          <div>
+            <h2 className="kadwa-regular text-lg ">Phone Number</h2>
+
+            <a href={`tel:${Phone}`}>
+              <h3 className="underline">{Phone}</h3>
+            </a>
+          </div>
         </div>
         <div className="flex gap-4">
           <FontAwesomeIcon
             icon={faEnvelope}
-            size="3x"
+            size="2x"
             className="text-red-800"
           />
-          <h2 className="kadwa-regular text-xl">Email Address</h2>
+          <div>
+            <h2 className="kadwa-regular text-lg">Email Address</h2>
+            <a href={`mailto:${SupportEmail}`}>
+              <h3 className="underline">{SupportEmail}</h3>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -1,17 +1,32 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import Divider from "~/components/Divider";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Excel Christian School" },
+    { name: "description", content: "Excel Christian School" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
+    <div className="flex flex-col items-center bg-[#f4f4e8] min-h-[100vh] p-4">
+      <div className="max-w-[1200px] flex flex-col items-center">
+        <h1 className="text-4xl my-4 uppercase text-serif">Welcome to Excel</h1>
+        <Divider />
+        <p className="text-lg mt-4">
+          We believe you’ll make an excellent decision by choosing Christian
+          education for your children, and we would be honored to be a part of
+          it. Our prayer is that together we will shape your children both
+          spiritually and academically, as well as strengthen their skills and
+          influence their world views. Although our world seems to be changing
+          at a rapid pace, the one constant we can all stand on is Jesus Christ.
+          He is the same yesterday, today and tomorrow! We are excited to not
+          only lead your children to a deeper understanding of who God is, but
+          also to equip them to reach their God-given potential.
+        </p>
+      </div>
       <Outlet />
     </div>
   );
